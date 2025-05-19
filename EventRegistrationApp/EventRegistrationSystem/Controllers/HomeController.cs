c30ee6fce3fd88ba9657af295c4911adceb81dcf
Piotr Sus <suotr12@gmail.com>
2025-05-19 13:33:00 +0200

    EventRegistrationSystem initial commit

---
+++ EventRegistrationApp/EventRegistrationSystem/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using EventRegistrationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventRegistrationSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
